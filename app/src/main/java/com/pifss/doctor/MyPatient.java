15f6d3587b5bea8068b10b1822c9b1060c35bddf
PIFSS <r9hacks@gmail.com>
2017-03-27 02:40:39 +0300

    Add listener to Each item in humburger, navigate to page on click upload new created files

---
+++ app/src/main/java/com/pifss/doctor/MyPatient.java
@@ -0,0 +1,13 @@
+package com.pifss.doctor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyPatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_patient);
+    }
+}
--- app/src/main/java/com/pifss/doctor/Navigation.java
+++ app/src/main/java/com/pifss/doctor/Navigation.java
@@ -1,5 +1,6 @@
 package com.pifss.doctor;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.annotation.ColorInt;
 import android.support.design.widget.TabLayout;
@@ -8,13 +9,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 import com.mikepenz.materialize.color.Material;
 
@@ -31,13 +35,13 @@ public class Navigation extends AppCompatActivity {
         toolbar.setTitleTextColor(Color.WHITE);
 
         PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("View Reports").withIcon(R.mipmap.medical_report_icon).withBadge("10");
-        PrimaryDrawerItem item2 = new PrimaryDrawerItem().withIdentifier(1).withName("Patient Requests").withIcon(R.mipmap.add_icon).withBadge("3");
-        PrimaryDrawerItem item3 = new PrimaryDrawerItem().withIdentifier(1).withName("My Patients").withIcon(R.mipmap.my_patient_icon).withBadge("7");
-        PrimaryDrawerItem item4 = new PrimaryDrawerItem().withIdentifier(1).withName("My Profile").withIcon(R.mipmap.doctor_profile_icon);
-        PrimaryDrawerItem item5 = new PrimaryDrawerItem().withIdentifier(1).withName("Share").withIcon(R.mipmap.share_icon);
-        PrimaryDrawerItem item6 = new PrimaryDrawerItem().withIdentifier(1).withName("Settings").withIcon(R.mipmap.settings_icon);
-        PrimaryDrawerItem item7 = new PrimaryDrawerItem().withIdentifier(1).withName("About us").withIcon(R.mipmap.aboutus_icon);
-        PrimaryDrawerItem item8 = new PrimaryDrawerItem().withIdentifier(1).withName("Logout").withIcon(R.mipmap.logout_icon);
+        PrimaryDrawerItem item2 = new PrimaryDrawerItem().withIdentifier(2).withName("Patient Requests").withIcon(R.mipmap.add_icon).withBadge("3");
+        PrimaryDrawerItem item3 = new PrimaryDrawerItem().withIdentifier(3).withName("My Patients").withIcon(R.mipmap.my_patient_icon).withBadge("7");
+        PrimaryDrawerItem item4 = new PrimaryDrawerItem().withIdentifier(4).withName("My Profile").withIcon(R.mipmap.doctor_profile_icon);
+        PrimaryDrawerItem item5 = new PrimaryDrawerItem().withIdentifier(5).withName("Share").withIcon(R.mipmap.share_icon);
+        PrimaryDrawerItem item6 = new PrimaryDrawerItem().withIdentifier(6).withName("Settings").withIcon(R.mipmap.settings_icon);
+        PrimaryDrawerItem item7 = new PrimaryDrawerItem().withIdentifier(7).withName("About us").withIcon(R.mipmap.aboutus_icon);
+        PrimaryDrawerItem item8 = new PrimaryDrawerItem().withIdentifier(8).withName("Logout").withIcon(R.mipmap.logout_icon);
         DividerDrawerItem d = new DividerDrawerItem();
 
 
@@ -57,7 +61,48 @@ public class Navigation extends AppCompatActivity {
 
         new DrawerBuilder().withActivity(this)
                 .withToolbar(toolbar)
-                .addDrawerItems(item1,item2,item3,item4,d,item5,item6,item7,item8)
+                .addDrawerItems(item1,item2,item3,item4,d,item5,item6,item7,item8).withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+            @Override
+            public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                Toast.makeText(Navigation.this, "position: "+position+" Identifier: "+drawerItem.getIdentifier(), Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent();
+                i.putExtra("empty","");
+                switch ((int) drawerItem.getIdentifier()){
+                    case 1:
+                        //current page
+                       // i = new Intent(Navigation.this,);
+                        break;
+                    case 2:
+                        i = new Intent(Navigation.this,PatientRequest.class);
+                        break;
+                    case 3:
+                        i = new Intent(Navigation.this,MyPatient.class);
+                        break;
+                    case 4:
+                        i = new Intent(Navigation.this,DoctorProfile.class);
+                        break;
+                    case 5:
+                        //Share
+                       // i = new Intent(Navigation.this);
+                        break;
+                    case 6:
+                        i = new Intent(Navigation.this,Settings.class);
+                        break;
+                    case 7:
+                        i = new Intent(Navigation.this,AboutUs.class);
+                        break;
+                    case 8:
+                        //Logout
+                        //i = new Intent(Navigation.this);
+                        break;
+                }
+                if (i.getExtras() == null) {
+                    startActivity(i);
+                }
+                return false;
+            }
+        })
                 .withAccountHeader(headerResult)
                 .build();
 
