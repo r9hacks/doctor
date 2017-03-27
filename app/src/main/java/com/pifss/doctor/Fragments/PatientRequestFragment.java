64e342e3daaa49844b6e6d9918792a91502f7fb1
Entisar <iamentisar@gmail.com>
2017-03-27 09:18:34 +0300

    put the links

--- app/src/main/java/com/pifss/doctor/Fragments/PatientRequestFragment.java
+++ app/src/main/java/com/pifss/doctor/Fragments/PatientRequestFragment.java
@@ -24,7 +24,15 @@ public class PatientRequestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_patient_request, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_patient_request, container, false);
+
+
+
+
+
+
+        return view;
     }
 
 }
