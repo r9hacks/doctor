0313ac6ff65e85d9070e6d489d2e7f9ddae0a93b
trn24 <r9hacks@gmail.com>
2017-03-28 12:18:27 +0300

    Updated register

--- app/src/main/java/com/pifss/doctor/Activitys/DoctorProfileActivity.java
+++ app/src/main/java/com/pifss/doctor/Activitys/DoctorProfileActivity.java
@@ -1,9 +1,14 @@
 package com.pifss.doctor.Activitys;
 
+
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
 
 import com.pifss.doctor.R;
 
@@ -21,7 +26,17 @@ public class DoctorProfileActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        Button btnnEdit = (Button) findViewById(R.id.btnEdit);
+
+        btnnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(DoctorProfileActivity.this,EditDoctorProfileActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
---
+++ app/src/main/java/com/pifss/doctor/Activitys/EditDoctorProfileActivity.java
@@ -0,0 +1,15 @@
+package com.pifss.doctor.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pifss.doctor.R;
+
+public class EditDoctorProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_doctor_profile);
+    }
+}
