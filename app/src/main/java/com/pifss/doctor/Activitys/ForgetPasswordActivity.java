1074540b31005b89cdcb45eb0a746c0566fe1a51
PIFSS <r9hacks@gmail.com>
2017-03-29 16:15:41 +0300

    ahmad added Forget password

--- app/src/main/java/com/pifss/doctor/Activitys/ForgetPasswordActivity.java
+++ app/src/main/java/com/pifss/doctor/Activitys/ForgetPasswordActivity.java
@@ -1,11 +1,29 @@
 package com.pifss.doctor.Activitys;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
+import com.pifss.doctor.Model.Doctor;
 import com.pifss.doctor.R;
+import com.pifss.doctor.RequestQueueSingleTon;
+import com.pifss.doctor.links;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ForgetPasswordActivity extends AppCompatActivity {
 
@@ -20,6 +38,43 @@ public class ForgetPasswordActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        Button sendEmail = (Button) findViewById(R.id.btnForget);
+        sendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RequestQueue queue= RequestQueueSingleTon.getInstance().getRequestQueue(ForgetPasswordActivity.this);
+
+                final EditText email = (EditText) findViewById(R.id.txtDrEmail);
+                try {
+
+                    JSONObject jsonBody = new JSONObject();
+                    jsonBody.put("email",email.getText().toString());
+
+
+                    JsonObjectRequest jsonObjRequest = new JsonObjectRequest(Request.Method.POST, links.resetPassword, jsonBody, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            System.out.println("reset password: "+response.toString());
+                            email.setText("");
+                            Toast.makeText(ForgetPasswordActivity.this, "reset password: "+response.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    });
+
+                    queue.add(jsonObjRequest);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
     }
 
     @Override
