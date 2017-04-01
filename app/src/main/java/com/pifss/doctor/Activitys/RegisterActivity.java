ad2e11e7978a09f5f9682ad9123656ac0c8362ed
PIFSS <r9hacks@gmail.com>
2017-04-01 18:46:29 +0300

    ahmad register new doctor

--- app/src/main/java/com/pifss/doctor/Activitys/RegisterActivity.java
+++ app/src/main/java/com/pifss/doctor/Activitys/RegisterActivity.java
@@ -34,9 +34,18 @@ import org.json.JSONObject;
 import java.io.UnsupportedEncodingException;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public static boolean validate(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(emailStr);
+        return matcher.find();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,7 +60,7 @@ public class RegisterActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        Button register = (Button) findViewById(R.id.btnRegister);
+        final Button register = (Button) findViewById(R.id.btnRegister);
         final EditText name = (EditText) findViewById(R.id.txtDrName);
         final EditText email = (EditText) findViewById(R.id.txtDrEmail);
         final EditText password = (EditText) findViewById(R.id.txtDrPassword);
@@ -61,14 +70,22 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (name.getText().toString() == "" || email.getText().toString() == "" || password.getText().toString() == "" || civilId.getText().toString() == ""){
-                   // Toast.makeText(RegisterActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                   Toast.makeText(RegisterActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 if (password.getText().toString().length() < 8){
-                   // Toast.makeText(RegisterActivity.this, "Password must be at least 8 character", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegisterActivity.this, "Password must be at least 8 character", Toast.LENGTH_SHORT).show();
                     return;
                 }
-               // Toast.makeText(RegisterActivity.this, "Connecting...", Toast.LENGTH_SHORT).show();
+
+                //validate email
+                if (validate(email.getText().toString()) == false){
+                    Toast.makeText(RegisterActivity.this, "Please enter a valid email address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Toast.makeText(RegisterActivity.this, "Connecting...", Toast.LENGTH_SHORT).show();
+
 
                 Doctor newDoctor = new Doctor(name.getText().toString(),email.getText().toString(),password.getText().toString(),civilId.getText().toString());
 
@@ -79,11 +96,26 @@ public class RegisterActivity extends AppCompatActivity {
                     JSONObject jsonBody = newDoctor.getJSONDoctor();
                     jsonBody.remove("drId");
                     System.out.println(jsonBody.toString());
-                    Toast.makeText(RegisterActivity.this, jsonBody.toString()+"", Toast.LENGTH_SHORT).show();
+
                     JsonObjectRequest jsonObjRequest = new JsonObjectRequest(Request.Method.POST, links.Doctor, jsonBody, new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(JSONObject response) {
-                            Toast.makeText(RegisterActivity.this, "on response: "+response.toString(), Toast.LENGTH_SHORT).show();
+                            try {
+                                if (response.getBoolean("status") == true){
+                                    Toast.makeText(RegisterActivity.this, "Register successfully", Toast.LENGTH_SHORT).show();
+                                    email.setText("");
+                                    name.setText("");
+                                    password.setText("");
+                                    civilId.setText("");
+                                }else{
+                                    Toast.makeText(RegisterActivity.this, "Register Failed", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(RegisterActivity.this, "Use another email address", Toast.LENGTH_SHORT).show();
+
+                                }
+                                //Toast.makeText(RegisterActivity.this, "on response: "+response.toString(), Toast.LENGTH_SHORT).show();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
 
                         }
                     }, new Response.ErrorListener() {
@@ -91,14 +123,23 @@ public class RegisterActivity extends AppCompatActivity {
                         public void onErrorResponse(VolleyError error) {
                             System.out.println("error: "+error.toString());
                             //show message
-                            Toast.makeText(RegisterActivity.this, "error response: "+error.toString(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterActivity.this, "Register Failed", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterActivity.this, "Use another email address", Toast.LENGTH_SHORT).show();
                         }
                     }){
                         @Override
                         protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
                             String jsonString = "";
                             JSONObject object = new JSONObject();
+
                             try {
+                                System.out.println("statuscode: "+response.statusCode);
+                                if (response.statusCode <200 || response.statusCode >300){
+
+                                    object.put("status",false);
+                                    return Response.success(object,
+                                            HttpHeaderParser.parseCacheHeaders(response));
+                                }
                                 jsonString = new String(response.data, "UTF-8");
                                 System.out.println("jsonString");
 
