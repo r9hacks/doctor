174cbab20c4265fbf3b0ced903398707ce35ec6a
PIFSS <r9hacks@gmail.com>
2017-04-11 08:05:20 +0300

    ahmad

---
+++ app/src/main/java/com/pifss/doctor/Activitys/MyBloodRequestsActivity.java
@@ -0,0 +1,15 @@
+package com.pifss.doctor.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pifss.doctor.R;
+
+public class MyBloodRequestsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_blood_requests);
+    }
+}
